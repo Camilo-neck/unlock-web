f7891c1b3cb48fababfe68ac34015fa8de920a3a
Camilo-neck <ccuello@unal.edu.co>
2024-09-21 22:47:44 -0500

    feat: add pagination

--- src/components/dashboard/eventAssistants.tsx
+++ src/components/dashboard/eventAssistants.tsx
@@ -3,7 +3,7 @@
 import { Input } from '../ui/input';
 import LoadUsersModal from './modals/LoadUsersModal';
 import { Button } from '../ui/button';
-import { Check, Logs, X } from 'lucide-react';
+import { Check, ChevronFirst, ChevronLast, ChevronLeft, ChevronRight, Logs, X } from 'lucide-react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
 import { formatDateString } from '@/lib/utils';
 import { Booking } from '@/schemas/booking.schema';
@@ -11,19 +11,45 @@ import useDashboardSidebars from '@/stores/useDashboardSidebars';
 import { useState } from 'react';
 import useBookings from '@/stores/useBookings';
 
+const ITEMS_PER_PAGE = 8;
+
 const EventAssistants = ({
 	eventId
 }: {
 	eventId: string;
 }) => {
-    const { toggleOpen } = useDashboardSidebars();
+	const { toggleOpen } = useDashboardSidebars();
 	const { bookings } = useBookings();
 	const [searchFilter, setSearchFilter] = useState<string>('');
+	const [currentPage, setCurrentPage] = useState<number>(1);
 
 	const filterBookings = (booking: Booking, filter: string) => {
 		return booking.user?.name?.toLowerCase().includes(filter.toLowerCase()) || booking.user.email.toLowerCase().includes(filter.toLowerCase());
 	}
 
+	const filteredBookings = bookings?.filter(booking => filterBookings(booking, searchFilter)) || [];
+	const totalPages = Math.ceil(filteredBookings.length / ITEMS_PER_PAGE);
+	const paginatedBookings = filteredBookings.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
+
+	const handleLastPage = () => {
+		setCurrentPage(totalPages);
+	}
+
+	const handleFirstPage = () => {
+		setCurrentPage(1);
+	}
+
+	const handlePreviousPage = () => {
+		if (currentPage > 1) {
+			setCurrentPage(currentPage - 1);
+		}
+	};
+
+	const handleNextPage = () => {
+		if (currentPage < totalPages) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
 
 	return (
 		<div className="bg-white rounded-lg shadow overflow-x-auto">
@@ -59,7 +85,7 @@ const EventAssistants = ({
 						</TableRow>
 					</TableHeader>
 					<TableBody>
-						{bookings?.filter(booking => filterBookings(booking, searchFilter)).map((booking, index) => (
+						{paginatedBookings.map((booking, index) => (
 							<TableRow key={booking.id}>
 								<TableCell className="font-medium">
 									{booking.user.name || '-'}
@@ -97,28 +123,36 @@ const EventAssistants = ({
 					</TableBody>
 				</Table>
 			</div>
-			{/* <div className="flex w-full items-center justify-between px-4 py-4 border-t">
+			<div className="flex w-full items-center justify-between px-4 py-4 border-t">
 				<div className="text-sm text-gray-500">
-					Showing 8 out of 100 entries
+					Showing {paginatedBookings.length} out of {filteredBookings.length} entries
 				</div>
 				<div className="flex space-x-2">
-					<Button variant="outline" size="sm">
-						Previous
+					<Button variant="outline" size="sm" onClick={handleFirstPage} disabled={currentPage === 1}>
+						<ChevronFirst size={18} />
 					</Button>
-					<Button variant="outline" size="sm">
-						1
+					<Button variant="outline" size="sm" onClick={handlePreviousPage} disabled={currentPage === 1}>
+						<ChevronLeft size={18} />
 					</Button>
-					<Button variant="outline" size="sm">
-						2
+					{Array.from({ length: totalPages }, (_, index) => (
+						<Button
+							key={index}
+							variant="outline"
+							size="sm"
+							onClick={() => setCurrentPage(index + 1)}
+							className={currentPage === index + 1 ? 'bg-gray-200' : ''}
+						>
+							{index + 1}
+						</Button>
+					))}
+					<Button variant="outline" size="sm" onClick={handleNextPage} disabled={currentPage === totalPages}>
+						<ChevronRight size={18} />
 					</Button>
-					<Button variant="outline" size="sm">
-						3
-					</Button>
-					<Button variant="outline" size="sm">
-						Next
+					<Button variant="outline" size="sm" onClick={handleLastPage} disabled={currentPage === totalPages}>
+						<ChevronLast size={18} />
 					</Button>
 				</div>
-			</div> */}
+			</div>
 		</div>
 	);
 };
